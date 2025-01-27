8ba3a45ff5293cb48d32fee3c245cb542864b7f2
christiangadelha <christiangadelha1@gmail.com>
2025-01-27 20:37:52 -0300

    Update en_words.py

--- en_words.py
+++ en_words.py
@@ -1,1 +1,221 @@
-a = 1
+temas = 
+  "brands": [
+        "apple", "microsoft", "google", "samsung", "amazon", "intel", "hp", "dell", "sony",
+        "facebook", "instagram", "twitter", "linkedin", "youtube", "whatsapp",
+        "mercedes", "bmw", "toyota", "volkswagen", "honda", "ferrari", "porsche",
+        "nike", "adidas", "puma", "converse", "vans", "ray-ban", "levi's",
+        "coca-cola", "pepsi", "fanta", "mcdonalds", "starbucks", "nestlé",
+        "netflix", "disney", "spotify", "playstation", "xbox", "nintendo",
+        "lg", "huawei", "canon", "philips", "casio", "asus", "lenovo",
+        "visa", "mastercard", "paypal", "uber", "airbnb", "booking",
+        "rolex", "gucci", "colgate", "gillette"
+],
+"feelings": [
+    "joy", "love", "happiness", "hope", "gratitude",
+    "affection", "trust", "enthusiasm", "pride", "compassion",
+    "sadness", "anger", "fear", "anxiety", "frustration",
+    "anguish", "disappointment", "jealousy", "despair", "loneliness",
+    "longing", "surprise", "nostalgia", "admiration", "serenity",
+    "optimism", "courage", "insecurity", "shame", "relief",
+    "euphoria", "passion", "panic", "fury", "torment",
+    "contentment", "tenderness", "curiosity", "fulfillment", "peace"
+],
+"elements": [ 
+    "Hydrogen", "Lithium", "Sodium", "Potassium", "Rubidium", "Cesium", "Francium",
+    "Beryllium", "Magnesium", "Calcium", "Strontium", "Barium", "Radium",
+    "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", 
+    "Copper", "Zinc", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", 
+    "Rhodium", "Palladium", "Silver", "Cadmium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", 
+    "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Rutherfordium", "Dubnium", 
+    "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium",
+    "Aluminum", "Gallium", "Indium", "Thallium", "Tin", "Lead", "Bismuth", "Antimony",
+    "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", 
+    "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium",
+    "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", 
+    "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", 
+    "Lawrencium", "Boron", "Silicon", "Germanium", "Arsenic", "Tellurium", "Polonium",
+    "Carbon", "Nitrogen", "Oxygen", "Phosphorus", "Sulfur", "Selenium",
+    "Fluorine", "Chlorine", "Bromine", "Iodine", "Astatine", "Tennessine",
+    "Helium", "Neon", "Argon", "Krypton", "Xenon", "Radon", "Oganesson"
+],
+"musical_instruments": [
+    "guitar", "piano", "drums", "flute", "guitar",
+    "violin", "bass", "trumpet", "keyboard", "harp",
+    "cello", "clarinet", "bassoon", "oboe", "viola",
+    "harmonica", "cavaquinho", "zither", "marimba", "tambourine",
+    "tamborim", "zabumba", "triangle", "snare drum", "timbale",
+    "cymbals", "tuba", "drum", "maracas", "berimbau",
+    "ocarina", "banjo", "clarinet", "xylophone", "recorder flute",
+    "accordion", "bagpipes", "dulcimer", "mandolin", "ukulele",
+    "saxophone", "didgeridoo", "pan flute", "theremin", "kalimba",
+    "lyre", "gong", "kazoo", "flute", "bongo"
+],
+'nature': [
+    "tree", "forest", "mountain", "waterfall", "ocean",
+    "desert", "plain", "valley", "river", "lake",
+    "volcano", "stone", "sand", "wind", "rain",
+    "snow", "hail", "lightning", "storm", "cloud",
+    "sky", "star", "moon", "sun", "field",
+    "garden", "grass", "leaf", "flower", "bush",
+    "fauna", "flora", "trail", "cliff", "coral",
+    "beach", "island", "wetland", "mangrove", "jungle",
+    "forest", "arid", "savanna", "frost", "ice",
+    "cave", "abyss", "swamp", "sunrise", "sunset",
+    "branch"
+],
+'sports': [
+    "soccer", "basketball", "volleyball", "tennis", "swimming",
+    "athletics", "cycling", "boxing", "karate", "judo",
+    "fencing", "gymnastics", "handball", "rugby", "cricket",
+    "hockey", "golf", "climbing", "skating", "surfing",
+    "rowing", "canoeing", "skateboarding", "snowboarding", "mountaineering",
+    "triathlon", "archery", "badminton", "baseball", "futsal",
+    "water polo", "kitesurfing", "motocross", "running", "high jump",
+    "pole vault", "hiking", "marathon", "weightlifting", "freestyle wrestling",
+    "ping pong", "shuttlecock", "bowling", "darts", "billiards",
+    "skiing", "taekwondo", "kickboxing", "windsurfing", "paragliding",
+    "horse racing"
+],
+"objects": [
+    "table", "chair", "sofa", "bed", "pillow",
+    "computer", "keyboard", "mouse", "monitor", "phone",
+    "cell phone", "television", "refrigerator", "stove", "microwave",
+    "blender", "fan", "lamp", "nightstand", "clock",
+    "mirror", "shelf", "book", "pen", "pencil",
+    "eraser", "notebook", "backpack", "bag", "wallet",
+    "glasses", "umbrella", "cup", "bottle", "plate",
+    "cutlery", "spoon", "fork", "knife", "pan",
+    "pillow", "rug", "comb", "brush", "shower",
+    "toaster", "iron", "hair dryer", "box", "drawer",
+    "closet", "shoe", "broom", "bucket", "ladder",
+    "battery", "remote control", "key", "padlock", "basket"
+],
+"animals": [
+    "dog", "cat", "elephant", "lion", "tiger",
+    "bear", "giraffe", "zebra", "hippopotamus", "rhinoceros",
+    "monkey", "bird", "fish", "turtle", "rabbit",
+    "horse", "ox", "sheep", "goat", "pig",
+    "rooster", "hen", "duckling", "turkey", "peacock",
+    "squirrel", "fox", "wolf", "polar bear", "panther",
+    "jaguar", "puma", "alligator", "snake", "lizard",
+    "crocodile", "spider", "ant", "bee", "fly",
+    "butterfly", "deer", "antelope", "camel", "buffalo",
+    "kangaroo", "penguin", "seal", "dolphin", "whale",
+    "shark", "bat", "starfish", "cow", "mule",
+    "seahorse", "crab", "mollusk", "shellfish"
+],
+"colors": [
+    "red", "blue", "green", "yellow", "orange",
+    "purple", "pink", "white", "black", "gray",
+    "brown", "beige", "violet", "indigo", "turquoise",
+    "chartreuse", "salmon", "purple", "cyan", "magenta",
+    "emerald", "saffron", "gold", "silver", "bronze",
+    "coral", "scarlet", "caramel", "teal", "lavender",
+    "snow-white", "navy blue", "light blue", "moss green", "mint green",
+    "lime green", "light yellow", "golden yellow", "pale yellow", "turquoise blue",
+    "dark blue", "dark turquoise", "forest green", "light purple", "light orange",
+    "aqua green", "pastel pink", "bright orange", "dark purple", "olive green",
+    "oil blue", "dark beige", "cobalt blue", "light gray", "dark gray",
+    "peach", "hot pink", "bright red", "eggplant", "light brown"
+],
+"countries": [
+    "Brazil", "United States", "Argentina", "France", "Italy",
+    "Germany", "Japan", "China", "Russia", "Mexico",
+    "Canada", "United Kingdom", "India", "Australia", "South Africa",
+    "Spain", "Portugal", "Colombia", "Poland", "Egypt",
+    "South Korea", "Sweden", "Switzerland", "Netherlands", "Norway",
+    "Turkey", "North Korea", "Iran", "Saudi Arabia", "Israel",
+    "Greece", "Turkey", "Thailand", "Indonesia", "Vietnam",
+    "New Zealand", "Pakistan", "Bangladesh", "Philippines", "Ireland",
+    "Czech Republic", "Hungary", "Austria", "Finland", "Denmark",
+    "Norway", "Belgium", "Romania", "Poland", "Ukraine",
+    "Iraq", "Syria", "Afghanistan", "Lebanon", "Kenya",
+    "Cuba", "Peru", "Chile", "Venezuela", "Bulgaria"
+],
+"games": [
+    "Minecraft", "The Sims", "Fortnite", "League of Legends", "World of Warcraft",
+    "Counter-Strike: Global Offensive", "Valorant", "Among Us", "Roblox", "Cyberpunk",
+    "The Witcher", "Red Dead Redemption", "Call of Duty: Warzone", "Grand Theft Auto", "Apex Legends",
+    "Hearthstone", "PUBG", "Rocket League", "Fall Guys", "Overwatch",
+    "Stardew Valley", "Terraria", "Factorio", "Civilization", "Dark Souls",
+    "The Elder Scrolls", "Genshin Impact", "Dota", "Rainbow Six Siege", "FIFA",
+    "NBA 2K", "Far Cry", "No Man's Sky", "Dead by Daylight", "Subnautica",
+    "Watch Dogs", "Battlefield", "Diablo", "Path of Exile", "Escape from Tarkov",
+    "Total War: Warhammer", "Tomb Raider", "The Forest", "Dead Space",
+    "Minecraft Dungeons", "Unravel", "Assassin's Creed", "Nier: Automata", "Control",
+    "Destiny", "Spelunky", "Super Meat Boy", "Cuphead", "Darkest Dungeon",
+    "Outer Worlds", "Hollow Knight", "Fallout", "Borderlands", "The Division"
+],
+"movies": [
+    "The Godfather", "The Lord of the Rings", "Harry Potter", "Star Wars", "Titanic",
+    "Avengers", "Jurassic Park", "The Matrix", "The Lion King", "Gladiator",
+    "Inception", "Interstellar", "The Origin", "The Exorcist", "Fight Club",
+    "Forrest Gump", "The Silence of the Lambs", "Pulp Fiction", "Citizen Kane", "The Big Lebowski",
+    "The Terminator", "Avatar", "Avengers: Endgame", "The Hobbit", "The Devil Wears Prada",
+    "The Wolf of Wall Street", "The Incredibles", "Shrek", "Pirates of the Caribbean", "The Dark Knight",
+    "Spirited Away", "Jurassic World", "Mad Max: Fury Road", "Django Unchained", "Frozen",
+    "Beauty and the Beast", "King Arthur", "The Pianist", "Pan's Labyrinth", "Wonder Woman",
+    "Schindler's List", "The Grand Budapest Hotel", "Doctor Strange", "The Imitation Game", "Joker",
+    "The Usual Suspects", "The Boy in the Striped Pajamas", "The Truman Show", "The Pursuit of Happyness", "White Chicks",
+    "La La Land", "Jurassic Park: The Lost World", "Léon: The Professional"
+],
+"series": [
+    "Game of Thrones", "Breaking Bad", "Stranger Things", "Friends", "The Office",
+    "The Crown", "The Mandalorian", "The Witcher", "Sherlock", "Black Mirror",
+    "Money Heist", "Narcos", "Peaky Blinders", "The Walking Dead", "The Sopranos",
+    "Westworld", "House of Cards", "True Detective", "Fargo", "Better Call Saul",
+    "The Boys", "The Haunting of Hill House", "Rick and Morty", "Dark", "Prison Break",
+    "The Flash", "Arrow", "Daredevil", "The Umbrella Academy", "How I Met Your Mother",
+    "The Big Bang Theory", "Friends", "Mindhunter", "Vikings", "The Expanse",
+    "Supernatural", "Doctor Who", "Hannibal", "Narcos: Mexico", "The Last Dance",
+    "Sex Education", "The Queen's Gambit", "The Handmaid's Tale", "The Good Place", "Orange is the New Black",
+    "Chernobyl", "The Marvelous Mrs. Maisel", "Lucifer", "Fleabag", "GLOW",
+    "The 100", "The Boys", "Black Sails", "The Punisher", "Loki", "Better Call Saul",
+    "Peaky Blinders", "Big Mouth", "Ted Lasso", "Narcos", "The Outsider"
+],
+"fruits": [
+    "apple", "banana", "orange", "grape", "pineapple",
+    "mango", "strawberry", "kiwi", "lemon", "watermelon",
+    "melon", "pear", "peach", "cherry", "plum",
+    "avocado", "raspberry", "blackberry", "passion fruit", "pitanga",
+    "carambola", "soursop", "acerola", "papaya", "tangerine",
+    "cashew", "jabuticaba", "fig", "pomegranate", "coconut",
+    "açaí", "persimmon", "nectarine", "guava", "jackfruit",
+    "pineapple guava", "lychee", "cupuaçu", "sour cherry", "physalis",
+    "sapodilla", "kumquat", "araticum", "bacuri", "buriti",
+    "umbu", "tamarind", "pitaya", "longan", "rambutan"
+],
+"professions": [
+    "doctor", "lawyer", "engineer", "teacher", "architect",
+    "programmer", "accountant", "designer", "nurse", "veterinarian",
+    "pharmacist", "psychologist", "journalist", "police officer", "firefighter",
+    "driver", "pilot", "chef", "waiter", "electrician",
+    "painter", "carpenter", "mechanic", "bricklayer", "welder",
+    "barber", "beautician", "photographer", "videographer", "actor",
+    "actress", "writer", "editor", "advertiser", "librarian",
+    "scientist", "astronomer", "chemist", "biologist", "physicist",
+    "historian", "geographer", "economist", "systems analyst", "IT technician",
+    "translator", "cabinetmaker", "goldsmith", "florist", "dubber"
+],
+"foods": [
+    "pizza", "hamburger", "lasagna", "sushi", "tacos",
+    "burrito", "risotto", "gnocchi", "pasta", "falafel",
+    "shawarma", "paella", "curry", "ramen", "tempura",
+    "ceviche", "quiche", "croissant", "baguette", "pita bread",
+    "barbecue", "roast chicken", "breaded steak", "fish and chips", "Caesar salad",
+    "gnocchi", "carbonara", "esfiha", "dumpling", "moqueca",
+    "guacamole", "pad thai", "fried rice", "kebab", "satay",
+    "waffles", "pancake", "crepe", "gelato", "brigadeiro",
+    "brownie", "chocolate mousse", "cheesecake", "pudding", "onion soup",
+    "apple pie", "carrot cake", "donuts", "churros", "tiramisu"
+]
+
+}
+
+
+
+
+
+
+
+
