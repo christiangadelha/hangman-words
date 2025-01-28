b221bdc296febb347255e301e5c6e2f23074e4b0
christiangadelha <christiangadelha1@gmail.com>
2025-01-28 11:59:53 -0300

    Update and rename translations.py to themes.py

---
+++ themes.py
@@ -0,0 +1,156 @@
+ThemesList = {
+    "random": {
+        "icon": r"Jogo\images\themes\icons\random.png",
+        "text": {
+            "Português-BR": "aleatório",
+            "English-US": "random",
+            "Español-ES": "aleatorio"
+        },
+        "selected": True
+    },
+    "foods": {
+        "icon": r"Jogo\images\themes\icons\foods.png",
+        "text": {
+            "Português-BR": "comidas",
+            "English-US": "foods",
+            "Español-ES": "alimentos"
+        },
+        "selected": False
+    },
+    "animals": {
+        "icon": r"Jogo\images\themes\icons\animals.png",
+        "text": {
+            "Português-BR": "animais",
+            "English-US": "animals",
+            "Español-ES": "animales"
+        },
+        "selected": False
+    },
+    "fruits": {
+        "icon": r"Jogo\images\themes\icons\fruits.png",
+        "text": {
+            "Português-BR": "frutas",
+            "English-US": "fruits",
+            "Español-ES": "frutas"
+        },
+        "selected": False
+    },
+    "countries": {
+        "icon": r"Jogo\images\themes\icons\countries.png",
+        "text": {
+            "Português-BR": "países",
+            "English-US": "countries",
+            "Español-ES": "países"
+        },
+        "selected": False
+    },
+    "colors": {
+        "icon": r"Jogo\images\themes\icons\colors.png",
+        "text": {
+            "Português-BR": "cores",
+            "English-US": "colors",
+            "Español-ES": "colores"
+        },
+        "selected": False
+    },
+    "professions": {
+        "icon": r"Jogo\images\themes\icons\professions.png",
+        "text": {
+            "Português-BR": "profissões",
+            "English-US": "professions",
+            "Español-ES": "profesiones"
+        },
+        "selected": False
+    },
+    "objects": {
+        "icon": r"Jogo\images\themes\icons\objects.png",
+        "text": {
+            "Português-BR": "objetos",
+            "English-US": "objects",
+            "Español-ES": "objetos"
+        },
+        "selected": False
+    },
+    "sports": {
+        "icon": r"Jogo\images\themes\icons\sports.png",
+        "text": {
+            "Português-BR": "esportes",
+            "English-US": "sports",
+            "Español-ES": "deportes"
+        },
+        "selected": False
+    },
+    "musical_instruments": {
+        "icon": r"Jogo\images\themes\icons\instruments.png",
+        "text": {
+            "Português-BR": "instrumentos musicais",
+            "English-US": "musical instruments",
+            "Español-ES": "instrumentos musicales"
+        },
+        "selected": False
+    },
+    "movies": {
+        "icon": r"Jogo\images\themes\icons\film.png",
+        "text": {
+            "Português-BR": "filmes",
+            "English-US": "movies",
+            "Español-ES": "películas"
+        },
+        "selected": False
+    },
+    "series": {
+        "icon": r"Jogo\images\themes\icons\series.png",
+        "text": {
+            "Português-BR": "séries",
+            "English-US": "series",
+            "Español-ES": "series"
+        },
+        "selected": False
+    },
+    "games": {
+        "icon": r"Jogo\images\themes\icons\games.png",
+        "text": {
+            "Português-BR": "jogos",
+            "English-US": "games",
+            "Español-ES": "juegos"
+        },
+        "selected": False
+    },
+    "feelings": {
+        "icon": r"Jogo\images\themes\icons\feelings.png",
+        "text": {
+            "Português-BR": "sentimentos",
+            "English-US": "feelings",
+            "Español-ES": "sentimientos"
+        },
+        "selected": False
+    },
+    "brands": {
+        "icon": r"Jogo\images\themes\icons\brands.png",
+        "text": {
+            "Português-BR": "marcas",
+            "English-US": "brands",
+            "Español-ES": "marcas"
+        },
+        "selected": False
+    },
+    "elements": {
+        "icon": r"Jogo\images\themes\icons\atom.png",
+        "text": {
+            "Português-BR": "tabela periódica",
+            "English-US": "periodic table",
+            "Español-ES": "tabla periódica"
+        },
+        "selected": False
+    },
+    "nature": {
+        "icon": r"Jogo\images\themes\icons\nature.png",
+        "text": {
+            "Português-BR": "natureza",
+            "English-US": "nature",
+            "Español-ES": "naturaleza"
+        },
+    },
+}
+
+
